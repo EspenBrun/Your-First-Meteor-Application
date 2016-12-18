0929b27e5fef3a0c9f918371324e2e04b1fa5ee8
Espen K. Brun <espen_brun@hotmail.com>
2016-12-18 19:01:36 +0100

    Done with Events

--- leaderboard.js
+++ leaderboard.js
@@ -11,6 +11,18 @@ if(Meteor.isClient){
 			return PlayersList.find().count()
 		}
 	});
+
+	Template.leaderboard.events({
+		'click .player': function(){
+			return console.log("you clicked a .player yo")
+		},
+		'dblclick .player': function(){
+			return console.log(".player dblclicked")
+		},
+		'mouseover .player': function(){
+			return console.log("just mousing over")
+		}
+	});
 }
 else if(Meteor.isServer){
 	console.log("Hello from js file to server");
